bdde6d360982c299c5e3ac58c0f8120a9cecb4dc
kshitij0304 <50038329+kshitij0304@users.noreply.github.com>
2019-06-09 13:21:21 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "This is my first script"
+
+read -p " Enter the value of A:"  var1
+
+read -p " Enter the option [Y/N] " choicevalue
+function1()
+{
+        df -h
+        ifconfig -a
+}
+if [ $var1 == 5 ]; then
+    echo "The input value is $var1"
+else 
+     echo "The value typed is not equal to 5: $var1"
+     exit 1
+fi
+
+if [ $choicevalue = "y" -o $choicevalue = "Y" ]; then
+     function1
+   else  
+     echo "Your choice is 'NO'"
+     exit 2
+fi
+         
